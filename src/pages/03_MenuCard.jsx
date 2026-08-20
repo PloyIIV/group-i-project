c4e734ebea1398d81cbc0fad8c9ed464ebef3e27
B6001186 <atcharapornok@gmail.com>
2026-08-20 13:58:02 +0700

    update menu card

--- src/pages/03_MenuCard.jsx
+++ src/pages/03_MenuCard.jsx
@@ -12,18 +12,37 @@ export const MenuCard = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center p-10 bg-white w-[80%] shadow-[0_0_15px_rgba(0,0,0,0.15)] rounded-2xl">
-        <div className="">
-          <img alt={menuDetails.name} src={menuDetails.pic_url}></img>
-        </div>
-        <div className="flex justify-between">
-          <h1 className="font-bold">{menuDetails.name}</h1>
-          <p className="font-bold text-xl">{menuDetails.price} ฿</p>
-        </div>
-        <div>
-          <button type="button" onClick={handleClickAddToCart}>
-            สั่งเลย
-          </button>
+      <div className="flex flex-col justify-center items-center p-10 bg-white w-[50%] shadow-[0_0_15px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div className="flex flex-col w-full gap-4 rounded-md">
+          <div className="">
+            <img
+              alt={menuDetails.name}
+              src={menuDetails.pic_url}
+              className="rounded-md"
+            ></img>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <h1 className="font-bold">{menuDetails.name}</h1>
+              <p>ระดับความเผ็ด</p>
+              {spicyLevel > 0 &&
+        Array.from({ length: spicyLevel }).map((_, index) => (
+          <span key={index}>🌶️</span>
+        ))}
+            </div>
+            <div className="">
+              <p className="font-bold text-2xl">{menuDetails.price} ฿</p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <button
+              className="font-bold bg-red-600 py-2 px-4 rounded-md text-white w-full"
+              type="button"
+              onClick={handleClickAddToCart}
+            >
+              สั่งเลย
+            </button>
+          </div>
         </div>
       </div>
     </div>

4b28c81f4e458ce58f4d7ff4979cd42e65698fdd
nookarinchai <arinnookynky@gmail.com>
2026-08-20 10:54:16 +0700

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from './components/Header.jsx'
+
 function App() {
-
-
   return (
     <>
-      
+      {/* Header is kept in its own component so it can be reused on every page. */}
+      <Header />
     </>
   )
 }

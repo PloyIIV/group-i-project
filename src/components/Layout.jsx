a81f7f8fce459205ceb3ac6e5decc38fb4ff48ba
PloyIIV <132745974+PloyIIV@users.noreply.github.com>
2026-08-20 11:11:46 +0700

    feat: add cart dropdown to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header.jsx"
+import CartDropdown from "./CartDropdown"
 
 const Layout = () => {
     return (
         <div>
             <Header />
-            <nav></nav>
+            <nav>
+                <CartDropdown />
+            </nav>
             <Outlet />
         </div>
     )
